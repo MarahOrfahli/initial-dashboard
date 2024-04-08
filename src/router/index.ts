a519cac7e3d6e332e8793f44b8f483e305563c0a
Marah Orfahli <marahorfahli7@gmail.com>
2024-04-08 11:49:52 +0300

    Start Project

---
+++ src/router/index.ts
@@ -0,0 +1,226 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { useAppStore } from '@/stores/index';
+import appSetting from '@/app-setting';
+
+import HomeView from '../views/index.vue';
+
+const routes: RouteRecordRaw[] = [
+    // dashboard
+    { path: '/', name: 'home', component: HomeView },
+    {
+        path: '/analytics',
+        name: 'analytics',
+        component: () => import(/* webpackChunkName: "analytics" */ '../views/analytics.vue'),
+    },
+    {
+        path: '/finance',
+        name: 'finance',
+        component: () => import(/* webpackChunkName: "finance" */ '../views/finance.vue'),
+    },
+    {
+        path: '/crypto',
+        name: 'crypto',
+        component: () => import(/* webpackChunkName: "crypto" */ '../views/crypto.vue'),
+    },
+
+    // apps
+    {
+        path: '/apps/chat',
+        name: 'chat',
+        component: () => import(/* webpackChunkName: "apps-chat" */ '../views/apps/chat.vue'),
+    },
+    {
+        path: '/apps/todolist',
+        name: 'todolist',
+        component: () => import(/* webpackChunkName: "apps-todolist" */ '../views/apps/todolist.vue'),
+    },
+    ////////////////////////////////
+    /* Main Categories */
+    {
+        path: '/apps/categories/main-categories/list',
+        name: 'main-categories-list',
+        component: () => import(/* webpackChunkName: "apps-invoice-list" */ '../views/apps/categories/main-categories/list.vue'),
+    },
+    // {
+    //     path: '/apps/invoice/preview',
+    //     name: 'invoice-preview',
+    //     component: () => import(/* webpackChunkName: "apps-invoice-preview" */ '../views/apps/invoice/preview.vue'),
+    // },
+    {
+        path: '/apps/categories/main-categories/add',
+        name: 'main-categories-add',
+        component: () => import(/* webpackChunkName: "apps-invoice-add" */ '../views/apps/categories/main-categories/add.vue'),
+    },
+    {
+        path: '/apps/categories/main-categories/edit',
+        name: 'main-categories-edit',
+        component: () => import(/* webpackChunkName: "apps-invoice-edit" */ '../views/apps/categories/main-categories/edit.vue'),
+    },
+
+
+    ////////////////////////
+    // invoice
+    {
+        path: '/apps/invoice/list',
+        name: 'invoice-list',
+        component: () => import(/* webpackChunkName: "apps-invoice-list" */ '../views/apps/invoice/list.vue'),
+    },
+    {
+        path: '/apps/invoice/preview',
+        name: 'invoice-preview',
+        component: () => import(/* webpackChunkName: "apps-invoice-preview" */ '../views/apps/invoice/preview.vue'),
+    },
+    {
+        path: '/apps/invoice/add',
+        name: 'invoice-add',
+        component: () => import(/* webpackChunkName: "apps-invoice-add" */ '../views/apps/invoice/add.vue'),
+    },
+    {
+        path: '/apps/invoice/edit',
+        name: 'invoice-edit',
+        component: () => import(/* webpackChunkName: "apps-invoice-edit" */ '../views/apps/invoice/edit.vue'),
+    },
+    {
+        path: '/apps/calendar',
+        name: 'calendar',
+        component: () => import(/* webpackChunkName: "apps-calendar" */ '../views/apps/calendar.vue'),
+    },
+    //charts
+    {
+        path: '/charts',
+        name: 'charts',
+        component: () => import(/* webpackChunkName: "charts" */ '../views/charts.vue'),
+    },
+    // users
+    {
+        path: '/users/profile',
+        name: 'profile',
+        component: () => import(/* webpackChunkName: "users-profile" */ '../views/users/profile.vue'),
+    },
+    {
+        path: '/users/user-account-settings',
+        name: 'user-account-settings',
+        component: () => import(/* webpackChunkName: "users-user-account-settings" */ '../views/users/user-account-settings.vue'),
+    },
+
+    // pages
+    {
+        path: '/pages/faq',
+        name: 'faq',
+        component: () => import(/* webpackChunkName: "pages-faq" */ '../views/pages/faq.vue'),
+    },
+    {
+        path: '/pages/coming-soon-boxed',
+        name: 'coming-soon-boxed',
+        component: () => import(/* webpackChunkName: "pages-coming-soon-boxed" */ '../views/pages/coming-soon-boxed.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/pages/coming-soon-cover',
+        name: 'coming-soon-cover',
+        component: () => import(/* webpackChunkName: "pages-coming-soon-cover" */ '../views/pages/coming-soon-cover.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/pages/error404',
+        name: 'error404',
+        component: () => import(/* webpackChunkName: "pages-error404" */ '../views/pages/error404.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/pages/error500',
+        name: 'error500',
+        component: () => import(/* webpackChunkName: "pages-error500" */ '../views/pages/error500.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/pages/error503',
+        name: 'error503',
+        component: () => import(/* webpackChunkName: "pages-error503" */ '../views/pages/error503.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/pages/maintenence',
+        name: 'maintenence',
+        component: () => import(/* webpackChunkName: "pages-maintenence" */ '../views/pages/maintenence.vue'),
+        meta: { layout: 'auth' },
+    },
+
+    // authentication
+    {
+        path: '/auth/boxed-signin',
+        name: 'boxed-signin',
+        component: () => import(/* webpackChunkName: "auth-boxed-signin" */ '../views/auth/boxed-signin.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/boxed-signup',
+        name: 'boxed-signup',
+        component: () => import(/* webpackChunkName: "auth-boxed-signup" */ '../views/auth/boxed-signup.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/boxed-lockscreen',
+        name: 'boxed-lockscreen',
+        component: () => import(/* webpackChunkName: "auth-boxed-lockscreen" */ '../views/auth/boxed-lockscreen.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/boxed-password-reset',
+        name: 'boxed-password-reset',
+        component: () => import(/* webpackChunkName: "auth-boxed-password-reset" */ '../views/auth/boxed-password-reset.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/cover-login',
+        name: 'cover-login',
+        component: () => import(/* webpackChunkName: "auth-cover-login" */ '../views/auth/cover-login.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/cover-register',
+        name: 'cover-register',
+        component: () => import(/* webpackChunkName: "auth-cover-register" */ '../views/auth/cover-register.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/cover-lockscreen',
+        name: 'cover-lockscreen',
+        component: () => import(/* webpackChunkName: "auth-cover-lockscreen" */ '../views/auth/cover-lockscreen.vue'),
+        meta: { layout: 'auth' },
+    },
+    {
+        path: '/auth/cover-password-reset',
+        name: 'cover-password-reset',
+        component: () => import(/* webpackChunkName: "auth-cover-password-reset" */ '../views/auth/cover-password-reset.vue'),
+        meta: { layout: 'auth' },
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkExactActiveClass: 'active',
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { left: 0, top: 0 };
+        }
+    },
+});
+
+router.beforeEach((to, from, next) => {
+    const store = useAppStore();
+
+    if (to?.meta?.layout == 'auth') {
+        store.setMainLayout('auth');
+    } else {
+        store.setMainLayout('app');
+    }
+    next(true);
+});
+router.afterEach((to, from, next) => {
+    appSetting.changeAnimation();
+});
+export default router;
---
+++ src/views/apps/categories/main-categories/list.vue
@@ -0,0 +1,154 @@
+<template>
+    <div>
+        <div class="panel px-0 pb-1.5 border-[#e0e6ed] dark:border-[#1b2e4b]">
+            <div class="datatable invoice-table">
+                <div class="mb-4.5 px-5 flex md:items-center md:flex-row flex-col gap-5">
+                    <div class="flex items-center gap-2">
+                        <button type="button" class="btn btn-danger gap-2" @click="deleteRow()">
+                            <icon-trash-lines />
+                            {{ $t('delete') }}
+                        </button>
+                        <router-link to="/apps/categories/main-categories/add" class="btn btn-primary gap-2">
+                            <icon-plus />
+                            {{ $t('add-new-section') }}
+                        </router-link>
+                    </div>
+                    <div class="ltr:ml-auto rtl:mr-auto">
+                        <input v-model="search" type="text" class="form-input" :placeholder="$t('search-ph')" />
+                    </div>
+                </div>
+
+                <vue3-datatable
+                    ref="datatable"
+                    :rows="items"
+                    :columns="cols"
+                    :totalRows="items?.length"
+                    :hasCheckbox="true"
+                    :sortable="true"
+                    :search="search"
+                    skin="whitespace-nowrap bh-table-hover"
+                    firstArrow='<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M13 19L7 12L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M16.9998 19L10.9998 12L16.9998 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>'
+                    lastArrow='<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M11 19L17 12L11 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path opacity="0.5" d="M6.99976 19L12.9998 12L6.99976 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg> '
+                    previousArrow='<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M15 5L9 12L15 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>'
+                    nextArrow='<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>'
+                >
+                    <template #titleArabic="data">
+                        <div>{{ data.value.titleArabic }}</div>
+                    </template>
+                    <template #titleEnglish="data">
+                        <div>{{ data.value.titleEnglish }}</div>
+                    </template>
+                    <template #catImg="data">
+                        <div class="flex items-center font-semibold">
+                            <div class="p-0.5 bg-white-dark/30 rounded-md w-max ltr:mr-2 rtl:ml-2">
+                                <img class="h-20 w-20 rounded-md object-cover" :src="`${data.value.catImg}`" />
+                            </div>
+                        </div>
+                    </template>
+                    <template #actions="data">
+                        <div class="flex gap-4 items-center justify-center">
+                            <router-link to="/apps/categories/main-categories/edit" class="hover:text-info">
+                                <icon-edit class="w-4.5 h-4.5" />
+                            </router-link>
+                            <button type="button" class="hover:text-danger" @click="deleteRow(`${data.value.id}`)">
+                                <icon-trash-lines />
+                            </button>
+                        </div>
+                    </template>
+                </vue3-datatable>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+    import { ref } from 'vue';
+    import { useI18n } from 'vue-i18n'
+    import Vue3Datatable from '@bhplugin/vue3-datatable';
+    import { useMeta } from '@/composables/use-meta';
+    import IconTrashLines from '@/components/icon/icon-trash-lines.vue';
+    import IconPlus from '@/components/icon/icon-plus.vue';
+    import IconEdit from '@/components/icon/icon-edit.vue';
+    import IconEye from '@/components/icon/icon-eye.vue';
+
+    useMeta({ title: 'Invoice List' });
+
+    const datatable: any = ref(null);
+    const { t, locale } = useI18n()
+    const search = ref('');
+    const cols = ref([
+        // { field: 'invoice', title: 'Invoice' },
+        { field: 'titleArabic', title: t('title-arabic') },
+        { field: 'titleEnglish', title: t('title-english')  },
+        { field: 'catImg', title: t('img')  },
+        { field: 'actions', title: t('action.name') , sort: false, headerClass: 'justify-center' },
+    ]);
+    const items = ref([
+        {
+            id: 1,
+            titleArabic: 'وجه',
+            titleEnglish: 'Face',
+            catImg: 'https://test.mightcinema.com/storage/images/categories/hqdelKiprK4sZbrRYcNTVrwCStQUeTV9798fcGDF.jpg'
+        },
+        {
+            id: 2,
+            titleArabic: 'وجه',
+            titleEnglish: 'Face',
+            catImg: 'https://test.mightcinema.com/storage/images/categories/hqdelKiprK4sZbrRYcNTVrwCStQUeTV9798fcGDF.jpg'
+        },
+        {
+            id: 3,
+            titleArabic: 'وجه',
+            titleEnglish: 'Face',
+            catImg: 'https://test.mightcinema.com/storage/images/categories/hqdelKiprK4sZbrRYcNTVrwCStQUeTV9798fcGDF.jpg'
+        },
+        {
+            id: 4,
+            titleArabic: 'وجه',
+            titleEnglish: 'Face',
+            catImg: 'https://test.mightcinema.com/storage/images/categories/hqdelKiprK4sZbrRYcNTVrwCStQUeTV9798fcGDF.jpg'
+        },
+    ]);
+    const searchText = ref('');
+    const columns = ref(['id', 'titleArabic', 'titleEnglish', 'catImg','actions']);
+    const tableOption = ref({
+        headings: {
+            id: (h: any, row: any, index: number) => {
+                return '#';
+            },
+        },
+        perPage: 10,
+        perPageValues: [10, 20, 30, 50, 100],
+        skin: 'table-hover',
+        columnsClasses: { actions: 'actions !text-center w-1' },
+        pagination: { show: true, nav: 'scroll', chunk: 10 },
+        texts: {
+            count: 'Showing {from} to {to} of {count} entries',
+            filter: '',
+            filterPlaceholder: 'Search...',
+            limit: '',
+        },
+        resizableColumns: false,
+        sortable: ['titleArabic', 'titleEnglish'],
+        sortIcon: {
+            base: 'sort-icon-none',
+            up: 'sort-icon-asc',
+            down: 'sort-icon-desc',
+        },
+    });
+
+    const deleteRow = (item: any = null) => {
+        if (confirm(t('check-delete'))) {
+            if (item) {
+                items.value = items.value.filter((d) => d.id != item);
+                datatable.value.clearSelectedRows();
+            } else {
+                let selectedRows = datatable.value.getSelectedRows();
+                const ids = selectedRows.map((d) => {
+                    return d.id;
+                });
+                items.value = items.value.filter((d) => !ids.includes(d.id as never));
+                datatable.value.clearSelectedRows();
+            }
+        }
+    };
+</script>
